57e7f89652fbd1a080ee9b5a74290f94f538547f
raja <abc@gmail.com>
2023-04-28 10:33:01 +0530

    new practise

---
+++ UnitTestProject1/TestScripts/Practice5.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+
+namespace UnitTestProject1.TestScripts
+{
+    [TestClass]
+    public class Practice5
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           IWebDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Url = "https://www.selenium.dev/";
+
+          var  element =driver.FindElement(By.XPath("//h4[text()='Selenium WebDriver']"));
+          var col = element.GetCssValue("color");
+            Console.WriteLine(col);
+        }
+    }
+}
--- UnitTestProject1/TestScripts/WebDriver.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace UnitTestProject1.TestScripts
-{
-    internal class ChromeDriver
-    {
-        public string Url { get; internal set; }
-    }
-}
