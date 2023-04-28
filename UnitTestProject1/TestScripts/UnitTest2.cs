3b014df8752f11e166c46ec40bd849754b5c1319
raja <abc@gmail.com>
2023-04-28 15:42:09 +0530

    new methods

---
+++ UnitTestProject1/TestScripts/UnitTest2.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.ObjectModel;
+
+namespace UnitTestProject1.TestScripts
+{
+    [TestClass]
+    public class UnitTest2
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Url = "https://www.facebook.com/";
+
+            driver.FindElement(By.XPath("//a[text()='Create new account']")).Click();
+
+            // var sel = driver.FindElement(By.Id("day"));
+
+
+           // var dayList = driver.FindElement(By.Name("birthday_day"));
+
+         //   SelectElement s1 = new SelectElement(dayList);
+           
+
+       var monList =driver.FindElement(By.XPath("//select[@name='birthday_month']"));
+          
+            SelectElement s2 = new SelectElement(monList);
+
+            
+
+          var  yearList =driver.FindElement(By.Name("birthday_year"));
+            SelectElement s3 =new SelectElement(yearList);
+
+            s3.SelectByText("2019");
+            s2.SelectByText("Jun");
+         //   s1.SelectByIndex(2);
+
+        }
+    }
+}
---
+++ UnitTestProject1/TestScripts/UnitTest4.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+
+namespace UnitTestProject1.TestScripts
+{
+    [TestClass]
+    public class UnitTest4
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+
+            driver.Url = "";
+
+
+
+
+        }
+    }
+}
