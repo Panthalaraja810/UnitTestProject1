45b467af3e70ca4fdd4b6443c9fdcd90d7aaa562
raja <abc@gmail.com>
2023-05-03 12:31:55 +0530

    DataDrivenTesting basic program commit

---
+++ UnitTestProject1/DataDrivenTesting/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using System.Reflection;
+
+namespace UnitTestProject1.DataDrivenTesting
+{
+
+   
+
+
+    [TestClass]
+    public class UnitTest1
+    {
+       
+        [TestMethod]
+        [TestCategory("DataDriven")]
+        [Priority(1)]
+        [DataTestMethod]
+        [DataRow("https://www.facebook.com/", "Facebook")]
+        [DataRow("https://medium.com/", "Medium")]
+        [DataRow("https://www.bbc.com/", "BBC")]
+        public void DataRowAndDataDriven(String url, String eTitle)
+        {
+            IWebDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver();
+            driver.Url = url;
+         String  aTitle = driver.Title;
+            try
+            {
+                Assert.IsTrue(aTitle.Contains(eTitle));
+            }
+            catch(Exception e) 
+            { 
+            Console.WriteLine(eTitle+" title not matching");
+                Console.WriteLine(e.StackTrace);
+            }
+
+
+        }
+    }
+}
---
+++ UnitTestProject1/DataDrivenTesting/UnitTest2.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UnitTestProject1.DataDrivenTesting
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        public static IEnumerable<object[]> Tests 
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] {1,1,2},
+                    new object[] {1,2,3},
+                    new object[] {2,2,4},
+                };
+            }
+
+        }
+
+        [TestMethod]
+        [TestCategory("DynamicData"),TestCategory("DataDriven")]
+        [DynamicData(nameof(Tests))]
+        public void TestMethod(int a, int b, int c)
+        {
+
+        }
+    }
+}
