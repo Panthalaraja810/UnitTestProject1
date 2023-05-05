a2edcf6518077c75d9e7d950bf1f7dbf26bcf801
raja <abc@gmail.com>
2023-05-05 15:48:43 +0530

    ExtentReport codes commi

---
+++ UnitTestProject1/GenericUtilities/ExtendReportUtilities.cs
@@ -0,0 +1,97 @@
+﻿using AventStack.ExtentReports;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestProject1.Main;
+
+namespace UnitTestProject1.GenericUtilities
+{
+
+    [TestClass]
+    public class ExtendReportUtilities : BaseCls
+    {
+        [TestInitialize]
+        public void extendReportsMethod()
+        {
+
+            driver =new ChromeDriver();
+
+        }
+
+        [TestMethod]
+        [TestCategory("ExtentHtmlReporter")]
+        public void Medium() 
+        {
+            extentTest =extentReports.CreateTest("Medium");
+
+            driver.Navigate().GoToUrl("https://medium.com/");
+
+           ITakesScreenshot takeScreenShot= (ITakesScreenshot)driver;
+
+           var screenShot = takeScreenShot.GetScreenshot();
+            screenShotPath = "C:\\Users\\panth\\source\\repos\\UnitTestProject1\\UnitTestProject1\\GenericUtilities\\ScreenShot\\Medium.png";
+            screenShot.SaveAsFile(screenShotPath, ScreenshotImageFormat.Png);
+
+             extentTest.Log(Status.Info, "medium screenshot");
+
+            bool condition = false;
+            if(condition==true)
+            {
+                Assert.IsTrue(true) ;
+                extentTest.Pass("test pass");
+            }
+            else
+            {
+                try
+                {
+                    Assert.IsTrue(false);
+                }
+                catch(Exception e)
+                {
+                    extentTest.Fail("test failed");
+                }
+
+
+            }
+
+        }
+
+        [TestMethod]
+        [TestCategory("ExtentHtmlReporter")]
+        public void instagram() 
+        {
+            extentTest = extentReports.CreateTest("Instagram");
+
+            driver.Navigate().GoToUrl("https://www.instagram.com/");
+
+            ITakesScreenshot takeScreenShot = (ITakesScreenshot)driver;
+
+            var screenShot = takeScreenShot.GetScreenshot();
+            screenShotPath = "C:\\Users\\panth\\source\\repos\\UnitTestProject1\\UnitTestProject1\\GenericUtilities\\ScreenShot\\Instagram.png";
+            screenShot.SaveAsFile(screenShotPath, ScreenshotImageFormat.Png);
+
+            extentTest.Log(Status.Warning, "Its is a status Waring");
+            extentTest.Warning("ExtentTestWarning Method");
+            Assert.IsTrue(true);
+            extentTest.Pass("TestPassed");
+
+
+        }
+
+        [TestCleanup]
+        public void extentClose()
+        {
+            driver.Close();
+            driver.Dispose();
+            extentTest.AddScreenCaptureFromPath(screenShotPath);    
+        }
+
+
+
+    }
+}
---
+++ UnitTestProject1/Main/BaseCls.cs
@@ -0,0 +1,40 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+
+namespace UnitTestProject1.Main
+{
+    [TestClass]
+    public class BaseCls
+    {
+       public IWebDriver driver;
+        public static ExtentReports extentReports;
+        public ExtentTest extentTest;
+        public string screenShotPath;
+      //  public static BaseCls instance=new BaseCls();
+        public static ExtentHtmlReporter extentHtmlReporter;
+        public static string testResultPath = "C:\\Users\\panth\\source\\repos\\UnitTestProject1\\UnitTestProject1\\GenericUtilities\\Reports\\";
+
+
+        [AssemblyInitialize]
+        public static void AssemblyInitializeMethod(TestContext testContext)
+        {
+            extentReports = new ExtentReports();
+
+            extentHtmlReporter = new ExtentHtmlReporter(testResultPath);
+            extentHtmlReporter.Start();
+            extentReports.AttachReporter(extentHtmlReporter);
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanupMethod()
+        {
+            extentReports.Flush();
+            extentHtmlReporter.Stop();
+        }
+
+
+    }
+}
